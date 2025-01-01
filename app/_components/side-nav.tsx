668ca54fe203aa26e377494d6fb34f5a1390448a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-01 22:04:40 +0200

    style: change side nav width and text size

--- app/_components/side-nav.tsx
+++ app/_components/side-nav.tsx
@@ -9,11 +9,11 @@ const SideNav = () => {
   const pathname = usePathname();
 
   return (
-    <div className='w-80 border-r-2 border-indigo-900 p-4 flex flex-col gap-2'>
+    <div className='border-r-2 border-indigo-900 p-4 flex flex-col gap-2 h-full text-xl w-96'>
       <Link
         href='/'
         className={cn(
-          'rounded-sm px-4 py-2 hover:bg-indigo-800',
+          'rounded-sm w-full px-4 py-2 hover:bg-indigo-800',
           pathname === '/' ? 'bg-indigo-900' : ''
         )}
       >
@@ -22,7 +22,7 @@ const SideNav = () => {
       <Link
         href='/charts'
         className={cn(
-          'rounded-sm px-4 py-2 hover:bg-indigo-800',
+          'rounded-sm w-full px-4 py-2 hover:bg-indigo-800',
           pathname === '/charts' ? 'bg-indigo-900' : ''
         )}
       >
