d0da166d9b306d0cadb2108f3f291db63c7ce416
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-01 22:02:02 +0200

    feat: add table to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { DataTable } from './data-table';
+import { columns } from './columns';
+import { data } from '@/data';
+
 export default function Home() {
-  return <div className=''>Home page</div>;
+  return (
+    <div className='p-6 flex flex-col items-center justify-center w-full'>
+      <h1>Dashboard</h1>
+      <Link href='/new'>
+        <Button variant='outline'>Add new</Button>
+      </Link>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
