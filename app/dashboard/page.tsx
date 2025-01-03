f488bd16f35303ced65dd03949c27f9c0d11e4aa
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-03 16:07:54 +0200

    feat: create designated dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { DataTable } from '../data-table';
+import { columns } from '../columns';
+import { data } from '@/data';
+
+export default function Dashboard() {
+  return (
+    <div className='p-6 flex flex-col items-center justify-center w-full gap-2 h-min'>
+      <Link href='/new' className='self-end'>
+        <Button variant='outline'>Add new</Button>
+      </Link>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import { DataTable } from './data-table';
-import { columns } from './columns';
-import { data } from '@/data';
-
 export default function Home() {
   return (
     <div className='p-6 flex flex-col items-center justify-center w-full gap-2'>
-      <Link href='/new' className='self-end'>
-        <Button variant='outline'>Add new</Button>
-      </Link>
-      <DataTable columns={columns} data={data} />
+      <h1>Home page</h1>
     </div>
   );
 }
