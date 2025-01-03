0940929e13e39f6bb0e85536c827c2de64d3619d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-03 17:11:18 +0200

    style: add fixed height to nav

--- app/_components/nav.tsx
+++ app/_components/nav.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className='w-full px-6 py-4 flex items-center justify-between border-b-2 border-indigo-900'>
+    <div className='w-full px-6 flex items-center justify-between border-b-2 border-indigo-900 h-16'>
       <Link href='/' className='font-bold'>
         Logo
       </Link>
