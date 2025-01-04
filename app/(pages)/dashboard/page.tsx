a09e73867ac8f55fb28728a223ffee0634c0db1e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-04 03:17:50 +0200

    chore: update imports path

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import { DataTable } from '../../data-table';
-import { columns } from '../../columns';
+import { DataTable } from './data-table';
+import { columns } from './columns';
 import { data } from '@/data';
 
 export default function Dashboard() {
