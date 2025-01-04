581e4f898bbbf91b174b4b362e04bff89ac36829
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-04 03:14:14 +0200

    style: update button padding and color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,19 +9,20 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-indigo-600 text-slate-100 hover:bg-indigo-500',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border-2 border-indigo-900 hover:bg-indigo-900',
+        outline:
+          'border-2 border-indigo-600 hover:bg-indigo-600 hover:text-slate-100',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: ' hover:text-indigo-500',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-1',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
+        default: 'py-2 px-4',
+        sm: 'py-1 rounded-md px-3',
+        lg: 'py-3 rounded-md px-8',
         icon: 'h-10 w-10',
       },
     },
