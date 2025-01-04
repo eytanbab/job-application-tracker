adf252334b369cde5e839b0847900660fd76cbe1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-04 03:13:34 +0200

    chore: refactor nav items

--- app/_components/side-nav.tsx
+++ app/_components/side-nav.tsx
@@ -8,26 +8,23 @@ import React from 'react';
 const SideNav = () => {
   const pathname = usePathname();
 
+  const navItems = ['dashboard', 'charts'];
   return (
     <div className='border-r-2 border-indigo-900 p-4 flex flex-col gap-2 h-full text-xl w-96'>
-      <Link
-        href='/dashboard'
-        className={cn(
-          'rounded-sm w-full px-4 py-2 hover:bg-indigo-800',
-          pathname === '/dashboard' ? 'bg-indigo-900' : ''
-        )}
-      >
-        Dashboard
-      </Link>
-      <Link
-        href='/charts'
-        className={cn(
-          'rounded-sm w-full px-4 py-2 hover:bg-indigo-800',
-          pathname === '/charts' ? 'bg-indigo-900' : ''
-        )}
-      >
-        Charts
-      </Link>
+      {navItems.map((item) => {
+        return (
+          <Link
+            key={item}
+            href={`/${item}`}
+            className={cn(
+              'rounded-sm w-full px-4 py-2 hover:bg-indigo-500 hover:text-slate-100 capitalize',
+              pathname === `/${item}` ? 'bg-indigo-600 text-slate-100' : ''
+            )}
+          >
+            {item}
+          </Link>
+        );
+      })}
     </div>
   );
 };
