8ede4d1b5c06fe990d9ce4d8fb545b8c921b9554
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-04 03:13:48 +0200

    chore: move dashboard page to pages folder

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { DataTable } from '../data-table';
-import { columns } from '../columns';
-import { data } from '@/data';
-
-export default function Dashboard() {
-  return <DataTable columns={columns} data={data} />;
-}
