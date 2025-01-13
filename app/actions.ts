9845c790a6c088a849dc4caee9ed3add9fe9274a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-13 21:04:28 +0200

    feat: wrap db calling in try catch block

--- app/actions.ts
+++ app/actions.ts
@@ -4,20 +4,26 @@ import { neon } from '@neondatabase/serverless';
 import { FormSchema } from './(pages)/new/page';
 import { auth } from '@clerk/nextjs/server';
 
+const sql = neon(process.env.DATABASE_URL!);
+
+export async function getApplications() {}
+
 export async function create(values: FormSchema) {
   const { userId } = await auth();
-
-  const sql = neon(process.env.DATABASE_URL!);
-  await sql(
-    'INSERT INTO job_applications (user_id, role_name, company_name, date_applied, link, platform, status) VALUES($1, $2, $3, $4, $5, $6, $7)',
-    [
-      userId,
-      values.role,
-      values.company,
-      values.date,
-      values.link,
-      values.platform,
-      values.status,
-    ]
-  );
+  try {
+    await sql(
+      'INSERT INTO job_applications (user_id, role_name, company_name, date_applied, link, platform, status) VALUES($1, $2, $3, $4, $5, $6, $7)',
+      [
+        userId,
+        values.role,
+        values.company,
+        values.date,
+        values.link,
+        values.platform,
+        values.status,
+      ]
+    );
+  } catch (err) {
+    throw err;
+  }
 }
