6565694153f714f5230a3b781fee8a8c5b895b4d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-13 21:27:08 +0200

    chore: add disable eslint for unused vars

--- app/(pages)/new/page.tsx
+++ app/(pages)/new/page.tsx
@@ -95,6 +95,7 @@ export default function New() {
         variant: 'default',
       });
       form.reset();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (err) {
       toast({
         description: 'Failed to submit application.',
