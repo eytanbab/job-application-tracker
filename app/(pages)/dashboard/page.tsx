278b7a612189105576a7b1348b14e73e9c0de44a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-14 02:44:02 +0200

    chore: remove unnecessary parentheses

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { getApplications } from '@/app/actions';
 
 export default async function Dashboard() {
   try {
-    const data = (await getApplications()) 
+    const data = await getApplications();
     return <DataTable columns={columns} data={data} />;
   } catch (err) {
     console.log('error fetching data from db.', err);
