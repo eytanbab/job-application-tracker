94bc3184ed31c0a282ab7d54e1a9dd7743fa2b84
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-14 15:18:59 +0200

    feat: add export schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,12 +1,16 @@
 import { pgTable, serial, varchar, text, date } from 'drizzle-orm/pg-core';
 
+import { createInsertSchema } from 'drizzle-zod';
+
 export const jobApplications = pgTable('job_applications', {
   id: serial('id').primaryKey(),
   userId: varchar('user_id', { length: 255 }).notNull(), // Clerk user ID
-  roleName: text('role_name').notNull(),
-  companyName: text('company_name').notNull(),
-  dateApplied: date('date_applied').notNull(),
+  role_name: text('role_name').notNull(),
+  company_name: text('company_name').notNull(),
+  date_applied: date('date_applied').notNull(),
   link: text('link'),
   platform: varchar('platform', { length: 255 }).notNull(),
   status: varchar('status', { length: 255 }).notNull(),
 });
+
+export const insertApplicationSchema = createInsertSchema(jobApplications);
