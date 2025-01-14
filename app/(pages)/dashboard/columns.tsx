688240cc39c5cbfe9aac76486d3137a1fc43e442
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-15 00:38:58 +0200

    refactor: change dropdown to buttons and form sheet on edit

--- app/(pages)/dashboard/columns.tsx
+++ app/(pages)/dashboard/columns.tsx
@@ -2,31 +2,28 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 
-import { MoreHorizontal, Pencil, Trash2, ArrowUpDown } from 'lucide-react';
+import { Pencil, Trash2, ArrowUpDown } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import Link from 'next/link';
 import { parseUrl } from 'next/dist/shared/lib/router/utils/parse-url';
-import { deleteApplication } from '@/app/actions';
+
+import { deleteApplication, updateApplication } from '@/app/actions';
 import { formatDate } from 'date-fns';
 import { toast } from '@/hooks/use-toast';
-import { redirect } from 'next/navigation';
 
-export type Application = {
-  id: string;
-  role_name: string;
-  company: string;
-  date_applied: string;
-  link: URL;
-  platform: string;
-  status: string;
-};
+import {
+  ApplicationForm,
+  FormValues,
+} from '@/app/_components/application-form';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
 
 const handleApplicationDelete = async (id: string) => {
   try {
@@ -37,7 +34,7 @@ const handleApplicationDelete = async (id: string) => {
   }
 };
 
-export const columns: ColumnDef<Application>[] = [
+export const columns: ColumnDef<FormValues>[] = [
   {
     accessorKey: 'role_name',
     header: ({ column }) => {
@@ -54,7 +51,7 @@ export const columns: ColumnDef<Application>[] = [
     },
   },
   {
-    accessorKey: 'company',
+    accessorKey: 'company_name',
     header: ({ column }) => {
       return (
         <Button
@@ -131,33 +128,53 @@ export const columns: ColumnDef<Application>[] = [
   {
     id: 'actions',
     cell: ({ row }) => {
+      // console.log('row:', row.original);
+      const onSubmit = async (values: FormValues) => {
+        try {
+          await updateApplication(values);
+          toast({
+            description: 'Application updated successfully!',
+            variant: 'default',
+          });
+
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+          toast({
+            description: 'Failed to submit application.',
+            variant: 'destructive',
+          });
+        }
+      };
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <MoreHorizontal className='h-4 w-4' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className='flex items-center justify-center w-fit bg-slate-100 text-indigo-600'>
-            <DropdownMenuItem className='flex gap-1 focus:text-indigo-400'>
-              {/* Implement edit functionality */}
-              <button
-                onClick={() =>
-                  redirect(`/dashboard/application/${row.original.id}`)
-                }
-              >
-                <Pencil className='size-6' />
+        <div className='flex gap-2 items-center'>
+          {/* Implement edit functionality */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <button className='hover:text-[#38CA8C]'>
+                <Pencil className='size-4' />
               </button>
-            </DropdownMenuItem>
-            <DropdownMenuItem className='flex gap-1 focus:text-[#F16366]'>
-              {/* Implement delete functionality */}
-              <button onClick={() => handleApplicationDelete(row.original.id)}>
-                <Trash2 className='size-6' />
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className='font-normal text-indigo-600'>
+                  Edit profile
+                </SheetTitle>
+              </SheetHeader>
+              {/* Edit */}
+              <ApplicationForm
+                defaultValues={row.original}
+                onSubmit={onSubmit}
+              />
+            </SheetContent>
+          </Sheet>
+          {/* Delete button */}
+          <button
+            className='hover:text-[#CA3876]'
+            onClick={() => handleApplicationDelete(row.original.id as string)}
+          >
+            <Trash2 className='size-4' />
+          </button>
+        </div>
       );
     },
   },
