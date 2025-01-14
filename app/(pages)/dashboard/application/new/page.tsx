62abad46eb7902f696256f170cfa37ac6cb1e5f2
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-14 15:21:18 +0200

    chore: refactor new application page

---
+++ app/(pages)/dashboard/application/new/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { z } from 'zod';
+
+import { createApplication } from '@/app/actions';
+import { useToast } from '@/hooks/use-toast';
+import { ApplicationForm } from '@/app/_components/application-form';
+import { insertApplicationSchema } from '@/app/db/schema';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const formSchema = insertApplicationSchema.omit({ userId: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+export default function NewApplication() {
+  const { toast } = useToast();
+
+  const defaultValues = {
+    role_name: '',
+    company_name: '',
+    date_applied: '',
+    link: '',
+    status: '',
+    platform: '',
+  };
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: FormValues) => {
+    try {
+      await createApplication(values);
+      toast({
+        description: 'Application submitted successfully!',
+        variant: 'default',
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      toast({
+        description: 'Failed to submit application.',
+        variant: 'destructive',
+      });
+    }
+  };
+
+  return <ApplicationForm defaultValues={defaultValues} onSubmit={onSubmit} />;
+}
