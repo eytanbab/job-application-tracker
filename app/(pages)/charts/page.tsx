3f25a916193cf7d9db5b37ab5eb33a5fdd0a1e38
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-23 19:19:30 +0200

    feat: add top 5 companies component to charts page

--- app/(pages)/charts/page.tsx
+++ app/(pages)/charts/page.tsx
@@ -1,3 +1,12 @@
-export default function Charts() {
-  return <div>Charts</div>;
+import { getTop5Companies } from '@/app/actions';
+import { CompaniesPieChart } from './components/companies-pie-chart';
+
+export default async function Charts() {
+  const companies = await getTop5Companies();
+
+  return (
+    <div className='w-full grid grid-cols-4 h-full'>
+      <CompaniesPieChart data={companies} />
+    </div>
+  );
 }
