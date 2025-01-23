b9a42db24f9dad886b85aac453483634ae414987
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-23 20:04:47 +0200

    feat: add statuses pie chart

--- app/(pages)/charts/page.tsx
+++ app/(pages)/charts/page.tsx
@@ -1,12 +1,16 @@
-import { getTop5Companies } from '@/app/actions';
-import { CompaniesPieChart } from './components/companies-pie-chart';
+import { getStatuses, getTop5Companies } from '@/app/actions';
+import { PieChartComponent } from './components/pie-chart';
+import { BarChartComponent } from './components/bar-chart';
 
 export default async function Charts() {
-  const companies = await getTop5Companies();
+  const top5Companies = await getTop5Companies();
+  const statuses = await getStatuses();
 
   return (
-    <div className='w-full grid grid-cols-4 h-full'>
-      <CompaniesPieChart data={companies} />
+    <div className='w-full grid grid-cols-4 h-full gap-4'>
+      <PieChartComponent title='Top 5 companies' data={top5Companies} />
+      <PieChartComponent title='Statuses' data={statuses} />
+      <BarChartComponent />
     </div>
   );
 }
