e37803c1da79b8a3120f6daa0ddfa8dc439b706c
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-01-31 14:49:35 +0200

    refactor: remove unnecessary button from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function Home() {
@@ -6,25 +5,30 @@ export default function Home() {
     <div className='p-6 flex flex-col items-center justify-center w-full gap-8'>
       <div className='flex flex-col gap-4'>
         <div>
-          <h1 className='text-8xl font-black uppercase '>Your job hunt</h1>
-          <h2 className='text-5xl font-black uppercase '>
+          <h1 className='text-8xl font-black uppercase'>Your job hunt</h1>
+          <h2 className='text-5xl font-black uppercase'>
             Organized and Effortless.
           </h2>
         </div>
-        <p className='max-w-2xl text-indigo-400 text-xl font-light leading-7'>
+        <p className='max-w-2xl text-indigo-500 text-xl font-normal leading-6'>
           Say goodbye to scattered notes and missed deadlines. Job Application
           Tracker helps you manage your applications, interviews, and follow-ups
           in one place.
         </p>
       </div>
       <div className='flex items-center gap-4'>
-        <Link href='/dashboard'>
-          <Button className='text-xl font-normal'>Start Tracking Today</Button>
+        <Link
+          href='/dashboard'
+          className='border-2 border-indigo-600 bg-indigo-600 text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 py-2 px-4 rounded-md'
+        >
+          Start Tracking Today
         </Link>
-
-        <Button variant='outline' className='text-xl font-normal'>
+        <Link
+          href='/placeholder'
+          className='border-2 border-indigo-600 hover:bg-indigo-600 hover:text-slate-100 py-2 px-4 rounded-md'
+        >
           See How It Works
-        </Button>
+        </Link>
       </div>
     </div>
   );
