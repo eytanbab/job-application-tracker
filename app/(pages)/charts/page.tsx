d7870addaa69ce06d5ce738c11391b59708d91fb
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-04 16:11:27 +0200

    refactor: replace Bar Chart with a custom one

--- app/(pages)/charts/page.tsx
+++ app/(pages)/charts/page.tsx
@@ -1,25 +1,24 @@
 import {
-  getApplicationsPerMonth,
+  getApplicationsPerYear,
   getTop5Statuses,
   getTop5Companies,
   getTop5Platforms,
   getYears,
 } from '@/app/actions';
 import { PieChartComponent } from './components/pie-chart';
-import { BarChartComponent } from './components/bar-chart';
+import { ApplicationsPerYearBarChart } from './components/applications-per-year-bar-chart';
 
 export default async function Charts() {
   const top5Companies = await getTop5Companies();
   const top5Statuses = await getTop5Statuses();
   const top5Platforms = await getTop5Platforms();
+  const applicationsPerYear = await getApplicationsPerYear();
   const years = await getYears();
-  const applicationsPerMonth = await getApplicationsPerMonth();
 
   // applicationsPerMonth.map((app) => {
   //   app['date_applied'] = getNameOfMonth(app['date_applied']);
   // });
 
-  console.log(years);
   return (
     <div className='w-full flex flex-col h-full gap-2'>
       {/* Overview Row */}
@@ -32,10 +31,7 @@ export default async function Charts() {
         <PieChartComponent title='Top 5 companies' data={top5Companies} />
         <PieChartComponent title='Top 5 platforms' data={top5Platforms} />
         <PieChartComponent title='Applications status' data={top5Statuses} />
-        <BarChartComponent
-          title='Applications status per month'
-          data={applicationsPerMonth}
-        />
+        <ApplicationsPerYearBarChart years={years} data={applicationsPerYear} />
       </div>
     </div>
   );
