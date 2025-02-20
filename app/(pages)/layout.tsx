e65fae1893b3a619a9c47d112c8ded6f1e54afa6
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-20 18:36:23 +0200

    style: remove overflow x hidden from layout

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -4,7 +4,7 @@ export default function PagesLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className='p-8 flex items-center justify-center w-full overflow-x-hidden'>
+    <main className='p-8 flex items-center justify-center w-full '>
       {children}
     </main>
   );
