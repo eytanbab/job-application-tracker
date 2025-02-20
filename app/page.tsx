70013ce8bfbaee17d3ca68633e32891d072abb63
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-20 17:44:14 +0200

    fix: update link href to applications page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </div>
       <div className='flex items-center gap-4'>
         <Link
-          href='/dashboard'
+          href='/applications'
           className='border-2 border-indigo-600 bg-indigo-600 text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 py-2 px-4 rounded-md'
         >
           Start Tracking Today
