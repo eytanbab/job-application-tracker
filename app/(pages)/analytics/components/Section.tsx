0b5fede2bf79be7f097af2eb0c653d250d5fca0b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-27 16:58:21 +0200

    refactor: simplify Section component

--- app/(pages)/analytics/components/Section.tsx
+++ app/(pages)/analytics/components/Section.tsx
@@ -1,16 +1,7 @@
 type Props = {
-  title: string;
   children: React.ReactNode;
 };
 
-export function Section({ title, children }: Props) {
-  return (
-    <>
-      <div className='w-full'>
-        <h1 className='text-2xl'>{title}</h1>
-        <div className='w-full h-px bg-slate-300 dark:bg-indigo-900' />
-      </div>
-      <div className='w-full flex gap-4'>{children}</div>
-    </>
-  );
+export function Section({ children }: Props) {
+  return <div className='w-full flex gap-4'>{children}</div>;
 }
