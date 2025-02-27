134eec7cc5a2bcc365ad7b980a308bdf2685a957
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-27 16:56:09 +0200

    chore: update url paths for nav items

--- app/_components/side-nav.tsx
+++ app/_components/side-nav.tsx
@@ -8,20 +8,29 @@ import React from 'react';
 const SideNav = () => {
   const pathname = usePathname();
 
-  const navItems = ['applications', 'analytics'];
+  const navItems = [
+    {
+      url: '/applications',
+      name: 'applications',
+    },
+    {
+      url: '/analytics/overview',
+      name: 'analytics',
+    },
+  ];
   return (
     <div className='border-r border-indigo-600/25 p-4 flex flex-col gap-2 text-xl w-96 sticky top-16 left-0 h-[calc(100vh-4rem)] bg-slate-100 dark:bg-slate-900 z-50'>
       {navItems.map((item) => {
         return (
           <Link
-            key={item}
-            href={`/${item}`}
+            key={item.url}
+            href={`${item.url}`}
             className={cn(
               'rounded-sm w-full px-4 py-2 hover:bg-indigo-500 hover:text-slate-100 capitalize transition-colors duration-300',
-              pathname === `/${item}` ? 'bg-indigo-600 text-slate-100' : ''
+              pathname.includes(item.name) ? 'bg-indigo-600 text-slate-100' : ''
             )}
           >
-            {item}
+            {item.name}
           </Link>
         );
       })}
