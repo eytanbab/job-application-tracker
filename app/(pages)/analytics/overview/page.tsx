d5d188dfbd8fa2ef24b0e8827484bb1bb35ddfd1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-27 16:57:02 +0200

    feat: add status per platform page

---
+++ app/(pages)/analytics/overview/page.tsx
@@ -0,0 +1,31 @@
+import {
+  getApplicationsPerYear,
+  getTop5Companies,
+  getTop5Platforms,
+  getTop5Statuses,
+  getYears,
+} from '@/app/actions';
+import { ApplicationsPerYearBarChart } from '../components/applications-per-year-bar-chart';
+import { PieChartComponent } from '../components/pie-chart';
+import { Section } from '../components/Section';
+
+export default async function Overview() {
+  const top5Companies = await getTop5Companies();
+  const top5Statuses = await getTop5Statuses();
+  const top5Platforms = await getTop5Platforms();
+
+  const applicationsPerYear = await getApplicationsPerYear();
+  const years = await getYears();
+
+  return (
+    <Section>
+      <PieChartComponent title='Top 5 companies' data={top5Companies} />
+      <PieChartComponent title='Top 5 platforms' data={top5Platforms} />
+      <PieChartComponent
+        title='Top 5 Applications status'
+        data={top5Statuses}
+      />
+      <ApplicationsPerYearBarChart years={years} data={applicationsPerYear} />
+    </Section>
+  );
+}
