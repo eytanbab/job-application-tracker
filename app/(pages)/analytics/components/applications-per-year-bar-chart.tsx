0b9f9fbd45c492ab45ea11314eab4e0e8af13704
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-27 15:22:18 +0200

    style: add max width to applications per year bar chart

--- app/(pages)/analytics/components/applications-per-year-bar-chart.tsx
+++ app/(pages)/analytics/components/applications-per-year-bar-chart.tsx
@@ -47,7 +47,7 @@ export function ApplicationsPerYearBarChart({ years, data }: Props) {
   }, [data, selectedYear]);
 
   return (
-    <Card className='flex-1'>
+    <Card className='flex-1  max-w-5xl'>
       <CardHeader className='w-full flex-row justify-between items-center'>
         <CardTitle>Applications per year</CardTitle>
         <Select value={selectedYear} onValueChange={setSelectedYear}>
