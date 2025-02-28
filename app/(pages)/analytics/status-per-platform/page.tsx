6c08c337eaa3bccfc29376c28585dcbaeee4ccc2
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-28 13:32:15 +0200

    style: add support for tablet view

--- app/(pages)/analytics/status-per-platform/page.tsx
+++ app/(pages)/analytics/status-per-platform/page.tsx
@@ -1,18 +1,16 @@
 import { getStatusPerPlatform } from '@/app/actions';
-import { Section } from '../components/Section';
 import { StatusPerPlatformPieChart } from '../components/status-per-platform-pie-chart';
 
 export default async function Overview() {
   const statusPerPlatform = await getStatusPerPlatform();
 
-  console.log(statusPerPlatform);
   if (statusPerPlatform.length === 0)
     return (
       <p>No applications found. Add an application to see the analytics.</p>
     );
 
   return (
-    <div className='col-span-full grid grid-cols-1 2xl:grid-cols-6 gap-4'>
+    <div className='col-span-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 gap-4'>
       {statusPerPlatform.map((platform) => {
         return (
           <StatusPerPlatformPieChart
