f6c3930d3846d1790d220530c56235d59f3ccbfe
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-28 13:32:25 +0200

    style: add support for tablet view

--- app/(pages)/analytics/components/Section.tsx
+++ app/(pages)/analytics/components/Section.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export function Section({ children }: Props) {
   return (
-    <div className='w-full grid grid-cols-1 2xl:grid-cols-5 gap-4'>
+    <div className='w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-5 gap-4'>
       {children}
     </div>
   );
