aed43eb5ce446485dcf7ce57a705ba92dc825f9f
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 02:40:01 +0200

    feat: add createFile action to add doc to neon db

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,11 @@ import { auth } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 
 import { db } from './db';
-import { insertApplicationSchema, jobApplications } from './db/schema';
+import {
+  documents,
+  insertApplicationSchema,
+  jobApplications,
+} from './db/schema';
 import { z } from 'zod';
 import { and, count, desc, eq } from 'drizzle-orm';
 
@@ -12,18 +16,13 @@ import { format } from 'date-fns';
 import { formatApplicationsPerYear } from '@/lib/utils';
 
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { ListObjectsV2Command, PutObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { s3Client } from '@/lib/s3-client';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const formSchema = insertApplicationSchema.omit({ userId: true });
 
 type FormValues = z.input<typeof formSchema>;
-
-const fileSchema = z.object({
-  fileName: z.string().min(2, 'File name is required'),
-  contentType: z.string().min(1, 'Content type is required'),
-});
 
 // Get all applications of current user
 export async function getApplications() {
@@ -251,6 +250,8 @@ export async function getStatusPerPlatform() {
   }));
 }
 
+/* ----------------- S3 BUCKET ------------------ */
+
 export async function generatePresignedUrl(
   fileName: string,
   contentType: string
@@ -276,9 +277,6 @@ export async function generatePresignedUrl(
       Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME || '', // Get the bucket name from env variable
       Key: fileKey,
       ContentType: contentType,
-      Metadata: {
-        userId, // Store userId as metadata
-      },
     };
 
     if (!uploadParams.Bucket) {
@@ -290,44 +288,26 @@ export async function generatePresignedUrl(
     const signedUrl = await getSignedUrl(s3Client, command, {
       expiresIn: 3600, // URL expiry time
     });
-
     return { signedUrl };
   } catch (error) {
     console.error('Error generating pre-signed URL:', error);
-    return { error: error.message };
+    return { error: (error as Error).message };
   }
+}
+
+export async function createFile(title: string, doc_url: string) {
+  const { userId } = await auth();
+  if (!userId) return;
+
+  return db
+    .insert(documents)
+    .values({ title, doc_url, userId })
+    .returning({ insertedId: documents.id });
 }
 
 export async function getFiles() {
   const { userId } = await auth();
   if (!userId) return;
 
-  try {
-    const listParams = {
-      Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME || '',
-      Prefix: `${userId}/`, // Filter files by userId (directory prefix)
-    };
-
-    if (!listParams.Bucket) {
-      throw new Error('AWS S3 bucket name is missing in environment variables');
-    }
-
-    // List the files in the bucket for the specific userId
-    const command = new ListObjectsV2Command(listParams);
-    const { Contents } = await s3Client.send(command);
-
-    if (!Contents) {
-      return []; // No files found
-    }
-
-    // Map through the files and return necessary info
-    return Contents.map((file) => ({
-      key: file.Key,
-      lastModified: file.LastModified,
-      size: file.Size,
-    }));
-  } catch (error) {
-    console.error('Error listing user files:', error);
-    return { error: error.message };
-  }
+  return db.select().from(documents).where(eq(documents.userId, userId));
 }
