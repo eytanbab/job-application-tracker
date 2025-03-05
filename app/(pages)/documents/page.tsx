6704afd613d72f96ebbbabfb1656eadbd52ea389
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 02:40:24 +0200

    feat: show files saved in db

--- app/(pages)/documents/page.tsx
+++ app/(pages)/documents/page.tsx
@@ -1,12 +1,25 @@
 import { getFiles } from '@/app/actions';
 import { FileUpload } from './components/file-upload';
+import Link from 'next/link';
 
 export default async function Documents() {
   const files = await getFiles();
   console.log(files);
+
   return (
-    <>
-      <FileUpload />;
-    </>
+    <div className='flex flex-col gap-4'>
+      <FileUpload />
+      <div>
+        {!files || files.length === 0 ? (
+          <p>No files uploaded.</p>
+        ) : (
+          files.map((file) => (
+            <Link key={file.id} href={file.doc_url}>
+              {file.title}
+            </Link>
+          ))
+        )}
+      </div>
+    </div>
   );
 }
