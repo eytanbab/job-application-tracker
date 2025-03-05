4d0b69f5ffecba4bec128a78d4cd27c2ca49aa3d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 20:28:59 +0200

    feat: add documents schema to db index

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,6 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { jobApplications } from './schema';
+import { documents, jobApplications } from './schema';
+
+console.log('DATABASE URL: ', process.env.DATABASE_URL);
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be a Neon postgres connection string');
@@ -8,5 +10,5 @@ if (!process.env.DATABASE_URL) {
 const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql, {
-  schema: { jobApplications },
+  schema: { jobApplications, documents },
 });
