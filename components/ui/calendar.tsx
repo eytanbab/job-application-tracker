c9378ac62b91ad3c4c329d097b831dc4851508c5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 02:59:46 +0200

    fix: resolve ts errors

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -80,7 +80,7 @@ function Calendar({
     }, [yearRange])
   );
 
-  const { onNextClick, onPrevClick, startMonth, endMonth } = props;
+  const { onPrevClick, startMonth, endMonth } = props;
 
   const columnsDisplayed = navView === 'years' ? 1 : numberOfMonths;
 
@@ -222,7 +222,6 @@ function Calendar({
         ),
         MonthGrid: ({ className, children, ...props }) => (
           <MonthGrid
-            children={children}
             className={className}
             displayYears={displayYears}
             startMonth={startMonth}
@@ -230,7 +229,9 @@ function Calendar({
             navView={navView}
             setNavView={setNavView}
             {...props}
-          />
+          >
+            {children}
+          </MonthGrid>
         ),
       }}
       numberOfMonths={columnsDisplayed}
@@ -317,7 +318,15 @@ function Nav({
     }
     goToMonth(previousMonth);
     onPrevClick?.(previousMonth);
-  }, [previousMonth, goToMonth]);
+  }, [
+    previousMonth,
+    goToMonth,
+    displayYears.from,
+    displayYears.to,
+    navView,
+    onPrevClick,
+    setDisplayYears,
+  ]);
 
   const handleNextClick = React.useCallback(() => {
     if (!nextMonth) return;
@@ -337,7 +346,16 @@ function Nav({
     }
     goToMonth(nextMonth);
     onNextClick?.(nextMonth);
-  }, [goToMonth, nextMonth]);
+  }, [
+    goToMonth,
+    nextMonth,
+    displayYears.from,
+    displayYears.to,
+    navView,
+    setDisplayYears,
+    onNextClick,
+  ]);
+
   return (
     <nav className={cn('flex items-center', className)}>
       <Button
