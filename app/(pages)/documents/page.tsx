2468f224fc0980244bb195aa858016349a4ef3fa
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 16:16:32 +0200

    refactor: add documents list component to documents page

--- app/(pages)/documents/page.tsx
+++ app/(pages)/documents/page.tsx
@@ -1,27 +1,11 @@
-import { getFiles } from '@/app/actions';
 import { FileUpload } from './components/file-upload';
-import Link from 'next/link';
+import { DocumentsList } from './components/documents-list';
 
 export default async function Documents() {
-  const files = await getFiles();
-  console.log(files);
-
   return (
     <div className='flex flex-col gap-4'>
       <FileUpload />
-      <div>
-        {!files || files.length === 0 ? (
-          <p>No files uploaded.</p>
-        ) : (
-          <div className='flex flex-col'>
-            {files.map((file) => (
-              <Link target='_blank' key={file.id} href={file.doc_url}>
-                {file.title}
-              </Link>
-            ))}
-          </div>
-        )}
-      </div>
+      <DocumentsList />
     </div>
   );
 }
