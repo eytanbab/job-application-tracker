00336934d81b2df842043d944c3f65593c970bbd
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-05 16:16:45 +0200

    feat: add documents list component

---
+++ app/(pages)/documents/components/documents-list.tsx
@@ -0,0 +1,19 @@
+import { getFiles } from '@/app/actions';
+import { Document } from './document';
+
+export const DocumentsList = async () => {
+  const docs = await getFiles();
+  return (
+    <div>
+      {!docs || docs.length === 0 ? (
+        <p>No files uploaded.</p>
+      ) : (
+        <div className='flex flex-col gap-2'>
+          {docs.map((file) => (
+            <Document key={file.id} file={file} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
