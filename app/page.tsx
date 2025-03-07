246c8a20c3a087eda26f5207a911792e6e3eb141
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-07 22:13:36 +0200

    refactor: change href to project readme

--- app/page.tsx
+++ app/page.tsx
@@ -21,13 +21,14 @@ export default function Home() {
       <div className='flex flex-col 2xl:flex-row items-center gap-4 w-full max-w-2xl'>
         <Link
           href='/applications'
-          className='border-2 border-indigo-600 bg-indigo-600 text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 py-2 px-4 rounded-md text-lg transition-colors duration-300 w-full text-center'
+          className='border-2 border-indigo-600 bg-indigo-600 text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 py-2 px-4 rounded-md text-normal 2xl:text-lg transition-colors duration-300 w-full text-center'
         >
           Start Tracking Today
         </Link>
         <Link
-          href='/placeholder'
-          className='border-2 border-indigo-600 hover:bg-indigo-600 hover:text-slate-100 py-2 px-4 rounded-md text-lg transition-colors duration-300 w-full text-center'
+          href='https://github.com/eytanbab/job-application-tracker/blob/master/README.md'
+          target='_blank'
+          className='border-2 border-indigo-600 hover:bg-indigo-600 hover:text-slate-100 py-2 px-4 rounded-md text-normal 2xl:text-lg transition-colors duration-300 w-full text-center'
         >
           See How It Works
         </Link>
