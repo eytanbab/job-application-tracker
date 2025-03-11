e3153c958a45ca0991a78749ef736b52f18e3fad
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-11 19:20:06 +0200

    chore: remove log

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,8 +2,6 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { documents, jobApplications } from './schema';
 
-console.log('DATABASE URL: ', process.env.DATABASE_URL);
-
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be a Neon postgres connection string');
 }
