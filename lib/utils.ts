812c930cd9e231e0bb976278c694528170551c62
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-03-13 18:15:03 +0200

    refactor: shorten months name

--- lib/utils.ts
+++ lib/utils.ts
@@ -60,18 +60,18 @@ export function formatApplicationsPerYear(data: Data[]) {
 }
 
 const MONTHS_MAP: Record<string, string> = {
-  '1': 'January',
-  '2': 'February',
-  '3': 'March',
-  '4': 'April',
+  '1': 'Jan',
+  '2': 'Feb',
+  '3': 'Mar',
+  '4': 'Apr',
   '5': 'May',
-  '6': 'June',
-  '7': 'July',
-  '8': 'August',
-  '9': 'September',
-  '10': 'October',
-  '11': 'November',
-  '12': 'December',
+  '6': 'Jun',
+  '7': 'Jul',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
 };
 
 export function transformApplicationsData(
