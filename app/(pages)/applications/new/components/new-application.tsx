5422d38a10439d5770669b180236827579278293
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-04-02 18:21:34 +0300

    refactor: move new application component to new file

---
+++ app/(pages)/applications/new/components/new-application.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { z } from 'zod';
+
+import { createApplication } from '@/app/actions/applications';
+import { useToast } from '@/hooks/use-toast';
+import { ApplicationForm } from '@/app/_components/application-form';
+import { insertApplicationSchema } from '@/app/db/schema';
+import { format } from 'date-fns';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const formSchema = insertApplicationSchema.omit({ userId: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+export default function NewApplication() {
+  const { toast } = useToast();
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: FormValues) => {
+    try {
+      await createApplication(values);
+      toast({
+        description: 'Application submitted successfully!',
+        variant: 'default',
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      toast({
+        description: 'Failed to submit application.',
+        variant: 'destructive',
+      });
+    }
+  };
+
+  const defaultValues = {
+    role_name: '',
+    company_name: '',
+    date_applied: format(Date.now(), 'yyyy-MM-dd'),
+    link: '',
+    description: '',
+    location: '',
+    status: '',
+    platform: '',
+    month: '',
+    year: '',
+  };
+
+  return (
+    <ApplicationForm
+      onClose={() => {}}
+      defaultValues={defaultValues}
+      onSubmit={onSubmit}
+    />
+  );
+}
