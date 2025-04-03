a1b346677a9ac0423f3f341516a4df21bcfa88ec
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-04-03 19:51:07 +0300

    style: add breakpoints and adjust grid columns

--- app/(pages)/analytics/status-per-platform/page.tsx
+++ app/(pages)/analytics/status-per-platform/page.tsx
@@ -16,7 +16,7 @@ export default async function Overview() {
     );
 
   return (
-    <div className='col-span-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 gap-4'>
+    <div className='col-span-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-6 gap-4'>
       {statusPerPlatform.map((platform) => {
         return (
           <StatusPerPlatformPieChart
