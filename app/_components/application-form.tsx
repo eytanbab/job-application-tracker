b65f5d7275a3cc5502369c963fc3f0c741d805b4
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-04-06 16:15:42 +0300

    feat: allow users to paste a job URL to auto-fill application details

--- app/_components/application-form.tsx
+++ app/_components/application-form.tsx
@@ -29,7 +29,8 @@ import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
 import { redirect } from 'next/navigation';
 import { Textarea } from '@/components/ui/textarea';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
+import { extractAiApplication } from '../actions/applications';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const createApplicationSchema = insertApplicationSchema.omit({
@@ -39,10 +40,27 @@ const createApplicationSchema = insertApplicationSchema.omit({
 
 export type FormValues = z.input<typeof createApplicationSchema>;
 
+const aiFormSchema = z.object({
+  url: z.string().url(),
+});
+
 type Props = {
   defaultValues: FormValues;
   onSubmit: (values: FormValues) => Promise<void>;
   onClose: () => void;
+};
+
+type AiValues = {
+  role_name: string;
+  company_name: string;
+  link: string;
+  platform: string;
+  status: string;
+  description: string;
+  location: string;
+  date_applied: Date | string;
+  month: string;
+  year: string;
 };
 
 export const ApplicationForm = ({
@@ -51,6 +69,7 @@ export const ApplicationForm = ({
   onClose,
 }: Props) => {
   const [isPending, startTransition] = useTransition();
+  const [aiValues, setAiValues] = useState<AiValues | null | undefined>(null);
 
   const formSchema = z.object({
     id: z.string().optional(),
@@ -75,6 +94,13 @@ export const ApplicationForm = ({
     status: z.string().min(2, {
       message: 'Status name must be at least 2 characters.',
     }),
+  });
+
+  const aiForm = useForm<z.infer<typeof aiFormSchema>>({
+    resolver: zodResolver(aiFormSchema),
+    defaultValues: {
+      url: '',
+    },
   });
 
   const form = useForm<FormValues>({
@@ -144,6 +170,51 @@ export const ApplicationForm = ({
     },
   ];
 
+  type AiAutoFill = {
+    status: 'success' | 'fail';
+    application: {
+      role_name: string;
+      company_name: string;
+      link: string;
+      platform: string;
+      status: 'Applied';
+      description: string;
+      location: string;
+    };
+  };
+
+  const handleAiSubmit = async (values: z.infer<typeof aiFormSchema>) => {
+    startTransition(async () => {
+      const aiAutoFill: AiAutoFill = await extractAiApplication(values.url);
+      console.log('aiAutoFill', aiAutoFill);
+
+      if (aiAutoFill === undefined || aiAutoFill.status === 'fail') {
+        setAiValues(undefined);
+        return;
+      }
+
+      const test: AiValues = {
+        date_applied: format(Date.now(), 'yyyy-MM-dd'),
+        role_name: aiAutoFill.application.role_name,
+        company_name: aiAutoFill.application.company_name,
+        link: aiAutoFill.application.link,
+        platform: aiAutoFill.application.platform,
+        status: 'Applied',
+        description: aiAutoFill.application.description,
+        location: aiAutoFill.application.location, // <- fix this too!
+        month: '',
+        year: '',
+      };
+
+      // ✅ Update form state so react-hook-form knows about the changes
+      Object.entries(test).forEach(([key, value]) => {
+        form.setValue(key, value);
+      });
+
+      setAiValues(test);
+    });
+  };
+
   const handleSubmit = (values: FormValues) => {
     // fix (production): date is selected as the previous day
     const formattedDate = format(values.date_applied, 'yyyy-MM-dd');
@@ -169,107 +240,225 @@ export const ApplicationForm = ({
   };
 
   // for debugging:
-  // console.log('Form current values:', form.watch());
-  // console.log('Form errors:', form.formState.errors);
+  console.log('Form current values:', form.watch());
+  console.log('Form errors:', form.formState.errors);
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(handleSubmit)}
-        className='flex flex-col w-full max-w-96 gap-2'
-      >
-        {fields.map((fld) => (
+    <div className='w-96 flex flex-col gap-2 items-center'>
+      <Form {...aiForm}>
+        <form
+          onSubmit={aiForm.handleSubmit(handleAiSubmit)}
+          className='flex flex-col w-full max-w-96 gap-2'
+        >
           <FormField
-            key={fld.name}
-            control={form.control}
-            name={fld.name}
-            render={({ field }) =>
-              // Render date field
-              fld.name === 'date_applied' ? (
-                <FormItem className='space-y-0'>
-                  <FormLabel>Date applied</FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <FormControl>
-                        <Button
-                          variant={'outline'}
-                          className={cn(
-                            'flex h-10 w-full rounded-md border border-indigo-600 dark:border-indigo-400 px-4 bg-transparent py-2 text-base font-normal disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-indigo-600 dark:text-indigo-100 group',
-                            !field.value && 'text-indigo-300'
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, 'PPP')
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                          <CalendarIcon className='ml-auto h-4 w-4 text-indigo-600 dark:text-indigo-400 group-hover:text-slate-50' />
-                        </Button>
-                      </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent
-                      className='w-auto p-0 bg-slate-100'
-                      align='start'
-                    >
-                      <Calendar
-                        mode='single'
-                        showYearSwitcher={false}
-                        selected={new Date(field.value!)}
-                        onSelect={field.onChange}
-                        disabled={(date: Date) =>
-                          date > new Date() || date < new Date('1900-01-01')
-                        }
-                      />
-                    </PopoverContent>
-                  </Popover>
-
-                  <FormMessage />
-                </FormItem>
-              ) : fld.name === 'description' ? (
-                <FormItem className='space-y-0'>
-                  <FormLabel>{fld.label}</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder={fld.placeholder}
-                      {...field}
-                      value={field.value as string}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              ) : (
-                // Render other fields
-                <FormItem className='space-y-0'>
-                  <FormLabel>{fld.label}</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder={fld.placeholder}
-                      {...field}
-                      value={field.value as string}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )
-            }
+            control={aiForm.control}
+            name='url'
+            render={({ field }) => (
+              <FormItem className='space-y-0'>
+                <FormLabel>URL</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='https://www.linkedin.com/jobs/view/123456789/'
+                    {...field}
+                  />
+                </FormControl>
+                {aiValues === undefined && (
+                  <p className='text-sm font-medium text-destructive dark:text-rose-500'>
+                    Failed to extract information from the URL.
+                  </p>
+                )}
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        ))}
-
-        <div className='mt-4 flex flex-col gap-2 w-full'>
           <Button type='submit' disabled={isPending}>
-            {isPending ? <Loader2 className='size-8 animate-spin' /> : 'Submit'}
+            {isPending ? (
+              <Loader2 className='size-8 animate-spin' />
+            ) : (
+              'Extract application'
+            )}
           </Button>
-          {/* Cancel button */}
-          <Button
-            type='button'
-            variant='outline'
-            onClick={onCancel}
-            disabled={isPending}
-          >
-            Cancel
-          </Button>
-        </div>
-      </form>
-    </Form>
+        </form>
+      </Form>
+      {/* Divider */}
+      <div className='w-full flex gap-1 items-center justify-center'>
+        <div className='w-full h-px bg-indigo-400'></div>
+        <span>OR</span>
+        <div className='w-full h-px bg-indigo-400'></div>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className='flex flex-col w-full max-w-96 gap-2'
+        >
+          {fields.map((fld) => (
+            <FormField
+              key={fld.name}
+              control={form.control}
+              name={fld.name}
+              render={({ field }) =>
+                // Render date field
+                fld.name === 'role_name' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'company_name' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'date_applied' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>Date applied</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant={'outline'}
+                            className={cn(
+                              'flex h-10 w-full rounded-md border border-indigo-600 dark:border-indigo-400 px-4 bg-transparent py-2 text-base font-normal disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-indigo-600 dark:text-indigo-100 group',
+                              !field.value && 'text-indigo-300'
+                            )}
+                          >
+                            {field.value ? (
+                              format(field.value, 'PPP')
+                            ) : (
+                              <span>Pick a date</span>
+                            )}
+                            <CalendarIcon className='ml-auto h-4 w-4 text-indigo-600 dark:text-indigo-400 group-hover:text-slate-50' />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent
+                        className='w-auto p-0 bg-slate-100'
+                        align='start'
+                      >
+                        <Calendar
+                          mode='single'
+                          showYearSwitcher={false}
+                          selected={new Date(field.value!)}
+                          onSelect={field.onChange}
+                          disabled={(date: Date) =>
+                            date > new Date() || date < new Date('1900-01-01')
+                          }
+                        />
+                      </PopoverContent>
+                    </Popover>
+
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'link' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'description' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'location' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'status' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : fld.name === 'platform' ? (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                ) : (
+                  <FormItem className='space-y-0'>
+                    <FormLabel>{fld.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={fld.placeholder}
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }
+            />
+          ))}
+
+          <div className='mt-4 flex flex-col gap-2 w-full'>
+            <Button type='submit' disabled={isPending}>
+              {isPending ? (
+                <Loader2 className='size-8 animate-spin' />
+              ) : (
+                'Submit'
+              )}
+            </Button>
+            {/* Cancel button */}
+            <Button
+              type='button'
+              variant='outline'
+              onClick={onCancel}
+              disabled={isPending}
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 };
