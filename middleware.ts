b91183c74573b561a5b2a2cbec868bda8d93504a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-04-08 17:53:52 +0300

    feat: add guest user handling with cookies

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,44 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 
 const isProtectedRoute = createRouteMatcher([
-  '/applications(.*)',
-  '/analytics(.*)',
-  '/documents(.*)'
+  // '/applications(.*)',
+  // '/analytics(.*)',
+  // '/documents(.*)'
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
+  const res = NextResponse.next();
+
+  if (!userId) {
+    const guestId = req.cookies.get('guest_id')?.value;
+
+    if (!guestId) {
+      const newGuestId = uuidv4();
+      res.cookies.set('guest_id', newGuestId, {
+        path: '/',
+        httpOnly: false,
+        sameSite: 'lax',
+        maxAge: 60 * 60 * 24 * 365, // 1 year
+      });
+    }
+  }
+
   // Redirect logged-in users from the homepage to /applications
-  if (userId && req.nextUrl.pathname === '/') {
+  if (req.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/applications', req.url));
   }
 
-  if (userId && req.nextUrl.pathname === '/analytics') {
+  if (req.nextUrl.pathname === '/analytics') {
     return NextResponse.redirect(new URL('/analytics/overview', req.url));
   }
 
   if (isProtectedRoute(req)) await auth.protect();
+
+  return res;
 });
 
 export const config = {
