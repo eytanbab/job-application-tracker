e55acf8354289eea9e07041d1a9d08d6f9d38f0e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-04-08 11:24:51 +0300

    refactor: make application field required in SuccessData

--- lib/types.ts
+++ lib/types.ts
@@ -23,7 +23,7 @@ export type ChartData = {
 /* ------ Open AI ------------*/
 type SuccessData = {
   status: 'success';
-  application?: FormValues;
+  application: FormValues;
 };
 
 type FailData = {
