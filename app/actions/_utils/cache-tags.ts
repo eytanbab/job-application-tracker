a300ff68aa3eebba146b219ddb5f775f7e37299a
eytanbab <73233191+eytanbab@users.noreply.github.com>
2026-02-13 01:21:18 +0200

    feat: create cache-tags.ts

---
+++ app/actions/_utils/cache-tags.ts
@@ -0,0 +1,4 @@
+export const CACHE_REVALIDATE_SECONDS = 300;
+
+export const applicationsTag = (userId: string) => `applications:${userId}`;
+export const documentsTag = (userId: string) => `documents:${userId}`;
