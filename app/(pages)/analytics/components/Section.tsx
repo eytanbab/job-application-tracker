792628287f9d5487e7bdf233f9a292b32384f014
eytanbab <73233191+eytanbab@users.noreply.github.com>
2026-02-18 15:40:34 +0200

    💄 style(Section): update grid layout styling
    - change order of class names for better readability
    - adjust gap size for improved spacing in the layout

--- app/(pages)/analytics/components/Section.tsx
+++ app/(pages)/analytics/components/Section.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export function Section({ children }: Props) {
   return (
-    <div className='w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 3xl:grid-cols-5 gap-4'>
+    <div className='grid w-full grid-cols-1 gap-3 md:grid-cols-2 2xl:grid-cols-4 3xl:grid-cols-5'>
       {children}
     </div>
   );
