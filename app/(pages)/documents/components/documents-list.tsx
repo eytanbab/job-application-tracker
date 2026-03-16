c90f8654256d1e0df0773f6e40219482165fe1df
eytanbab <73233191+eytanbab@users.noreply.github.com>
2026-03-16 23:16:35 +0200

    refactor(documents): simplify documents list rendering logic - move no files message check outside of return statement - use consistent quotation marks for imports

--- app/(pages)/documents/components/documents-list.tsx
+++ app/(pages)/documents/components/documents-list.tsx
@@ -1,19 +1,17 @@
-import { getFiles } from '@/app/actions/documents';
-import { Document } from './document';
+import { getFiles } from "@/app/actions/documents";
+import { Document } from "./document";
 
 export const DocumentsList = async () => {
   const docs = await getFiles();
+  if (!docs || docs.length === 0) {
+    return <p>No files uploaded.</p>;
+  }
+
   return (
-    <div>
-      {!docs || docs.length === 0 ? (
-        <p>No files uploaded.</p>
-      ) : (
-        <div className='flex gap-2 flex-wrap justify-start'>
-          {docs.map((file) => (
-            <Document key={file.id} file={file} />
-          ))}
-        </div>
-      )}
+    <div className="flex gap-2 flex-wrap justify-start">
+      {docs.map((file) => (
+        <Document key={file.id} file={file} />
+      ))}
     </div>
   );
 };
