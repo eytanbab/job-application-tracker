bf6bd6ab65002a6c468c045f8993faac569ed2ba
eytanbab <73233191+eytanbab@users.noreply.github.com>
2026-08-11 16:30:45 +0300

    chore(mobile-nav): remove add application button

--- app/_components/side-nav-mobile.tsx
+++ app/_components/side-nav-mobile.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Sheet,
@@ -6,14 +6,14 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet';
-import { cn, navItems } from '@/lib/utils';
-import { Menu } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+} from "@/components/ui/sheet";
+import { cn, navItems } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
-import { NewApplicationButton } from './new-application-button';
+import { NewApplicationButton } from "./new-application-button";
 
 export function MobileSideNav() {
   const [open, setOpen] = useState(false);
@@ -21,16 +21,14 @@ export function MobileSideNav() {
 
   return (
     <Sheet open={open} onOpenChange={setOpen}>
-      <SheetTrigger asChild className='2xl:hidden cursor-pointer'>
+      <SheetTrigger asChild className="2xl:hidden cursor-pointer">
         <Menu />
       </SheetTrigger>
-      <SheetContent className='w-full flex flex-col gap-2 h-full items-center justify-center '>
+      <SheetContent className="w-full flex flex-col gap-2 h-full items-center justify-center ">
         <SheetHeader>
           <SheetTitle></SheetTitle>
         </SheetHeader>
-        <div className="w-full mb-2" onClick={() => setOpen(false)}>
-          <NewApplicationButton fullWidth label="New Application" />
-        </div>
+
         {navItems.map((item) => {
           return (
             <Link
@@ -38,10 +36,10 @@ export function MobileSideNav() {
               href={`${item.url}`}
               onClick={() => setOpen(false)}
               className={cn(
-                'w-full rounded-md px-4 py-2 text-lg capitalize text-muted-foreground transition-colors duration-200 hover:bg-accent/70 hover:text-foreground',
+                "w-full rounded-md px-4 py-2 text-lg capitalize text-muted-foreground transition-colors duration-200 hover:bg-accent/70 hover:text-foreground",
                 pathname.includes(item.name)
-                  ? 'border border-border/60 bg-primary/15 text-foreground shadow-sm'
-                  : ''
+                  ? "border border-border/60 bg-primary/15 text-foreground shadow-sm"
+                  : "",
               )}
             >
               {item.name}
